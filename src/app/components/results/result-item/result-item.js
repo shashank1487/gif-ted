f139b721a96e5986ae2be302ba360ab6c28db2d5
Shashank <shashank@Neuadmins-MacBook-Pro.local>
2019-06-02 17:32:22 +0530

    added styling for result item

--- src/app/components/results/result-item/result-item.js
+++ src/app/components/results/result-item/result-item.js
@@ -2,9 +2,10 @@ import React from "react";
 
 const ResultItem = props => {
   const { id, images } = props;
+
   return (
     <div className="item">
-      <img key={id} src={images.original.url} />
+      <img key={id} src={images.fixed_width.url} className="gif" />
     </div>
   );
 };
